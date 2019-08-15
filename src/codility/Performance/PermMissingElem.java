46d603c9dfcac1769a2099a725f9a3db4265a8a1
Joseph, Viju <vijujoseph@gmail.com>
2019-08-15 12:12:48 +0530

    commit - Codility - Performance missing element

--- src/codility/Performance/PermMissingElem.java
+++ src/codility/Performance/PermMissingElem.java
@@ -2,6 +2,20 @@ package codility.Performance;
 
 import java.util.Arrays;
 
+/*
+For example, given array A such that:
+
+A[0] = 2
+A[1] = 3
+A[2] = 1
+A[3] = 5
+the function should return 4, as it is the missing element.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [0..100,000];
+the elements of A are all distinct;
+each element of array A is an integer within the range [1..(N + 1)].*/
 public class PermMissingElem {
 
     public static void main(String args[]) {
